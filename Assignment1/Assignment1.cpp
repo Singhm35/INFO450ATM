e603209a876d223e939b81862ae36b7a3e16ee2e
Manvir Singh <singhm35@vcu.edu>
2018-09-22 18:38:22 -0400

    Commit 2

---
+++ Assignment1/Assignment1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int x;
+	//This is if the user wants to repeat. It is a while loop
+	char again = 'Y';
+	while (again == 'Y')
+	{
+	cout << "Enter withdrawl amount:" << endl;
+	cin >> x;
+	//How the demoinations are chosen
+	if (x <= 300 && x >= 1) {
+		int y;
+		y = x;
+		cout << "You have " << y / 50 << "$50.00 bills"<< endl;
+		y = y % 50;
+		cout << "You have " << y / 20 << "$20.00 bills" << endl;
+		y = y % 20;
+		cout << "You have " << y / 10 << "$10.00 bills" << endl;
+		y = y % 10;
+		cout << "You have " << y / 5 << "$5.00 bills" << endl;
+		y = y % 5;
+		cout << "You have " << y / 1 << "$1.00 bills" << endl;
+	}
+	else {
+		//results of the given amounts.
+		cout << "Sorry the max/min amount you can withdraw is $300.00/$1.00" << endl;
+
+	}//end the while loop
+	cout << "Another withdrawl? (Enter Y to continue)";
+	cin >> again;
+	
+		return 0;
+		cout << "The end." << endl;
+	}
+}
